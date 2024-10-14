ed4bdd886ed7ddf0ad2cc600c50e345e1f47063e
Nguyễn Văn Minh <161409681+vminhuit05@users.noreply.github.com>
2024-10-14 23:36:46 +0700

    Update bai2.h

--- BAI2/bai2.h
+++ BAI2/bai2.h
@@ -3,30 +3,17 @@
 
 class TamGiac {
 private:
-    double x1, y1; // Coordinates of the first vertex
-    double x2, y2; // Coordinates of the second vertex
-    double x3, y3; // Coordinates of the third vertex
+    double x1, y1;
+    double x2, y2;
+    double x3, y3;
 
 public:
-    // Constructor
     TamGiac();
-
-    // Method to input the vertices of the triangle
     void Nhap();
-
-    // Method to output the vertices of the triangle
     void Xuat();
-
-    // Method to translate the triangle
     void TinhTien(double dx, double dy);
-
-    // Method to scale up the triangle
     void PhongTo(double scale);
-
-    // Method to scale down the triangle
     void ThuNho(double scale);
-
-    // Method to rotate the triangle
     void Quay(double angle);
 };
 
