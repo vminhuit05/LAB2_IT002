ec360827849091fe20f0a4355847acdb4f5ed50e
Nguyễn Văn Minh <161409681+vminhuit05@users.noreply.github.com>
2024-10-14 23:43:21 +0700

    Update bai3.h

--- BAI3/bai3.h
+++ BAI3/bai3.h
@@ -5,39 +5,18 @@ class Diem {
 private:
     double Hoanh;
     double Tung;
+
 public:
-//constructor ko tham so
-    Diem() {
-        Hoanh = 0;
-        Tung = 0;
-    }
-
-//constructor co tham so
-    Diem(double Hoanh, double Tung) {
-        this -> Hoanh = Hoanh;
-        this -> Tung = Tung;
-    }
-
-//copy constructor
-    Diem(const Diem &x) {
-        Hoanh = x.Hoanh;
-        Tung = x.Tung;
-    }
+    Diem();
+    Diem(double Hoanh, double Tung);
+    Diem(const Diem &x);
 
     void Xuat();
-
     double GetTungDo();
-
     double GetHoanhDo();
-
     void SetTungDo(double Tung);
-
     void SetHoanhDo(double Hoanh);
-
-//tinh tien diem di 1 doan delta x va 1 doan delta y
     void TinhTien(double dx, double dy);
-
-//quay 1 diem quanh goc toa do
     void Quay(double rad);
 };
 
@@ -47,24 +26,12 @@ private:
     Diem *Dinh;
 
 public:
-    DaGiac() {
-        n = 0;
-        Dinh = nullptr;
-    }
-
+    DaGiac();
+    ~DaGiac();
     void Nhap();
-
     void Xuat();
-
-// tinh tien di theo mot vector (dx, dy)
     void TinhTien(double dx, double dy);
-
-// phong to theo 1 scale
     void PhongTo(double scale);
-
-// thu nho theo 1 scale
     void ThuNho(double scale);
-
-//quay 1 goc rad quanh goc toa do
     void Quay(double radian);
 };
