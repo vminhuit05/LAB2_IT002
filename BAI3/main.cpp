0d872d763af75af6acd891b7e0ca591e10180bf1
Nguyễn Văn Minh <161409681+vminhuit05@users.noreply.github.com>
2024-10-14 23:42:50 +0700

    Update main.cpp

--- BAI3/main.cpp
+++ BAI3/main.cpp
@@ -1,15 +1,18 @@
 #include <bits/stdc++.h>
-#include "bai3.cpp"
+#include "bai3.h"
 using namespace std;
 
 int main() {
     DaGiac dg;
     double x;
+
     dg.Nhap();
     dg.Xuat();
 
     cout << "Nhap lan luot dx va dy de tinh tien: ";
-    dg.TinhTien(2, 3);
+    double dx, dy;
+    cin >> dx >> dy;
+    dg.TinhTien(dx, dy);
     cout << "\nDa giac sau khi tinh tien: " << endl;
     dg.Xuat();
 
@@ -19,13 +22,13 @@ int main() {
     cout << "\nDa giac sau khi phong to: " << endl;
     dg.Xuat();
 
-    cout << "Nhap scale thu nho dagiac: ";
+    cout << "Nhap scale thu nho da giac: ";
     cin >> x;
     dg.ThuNho(x);
     cout << "\nDa giac sau khi thu nho: " << endl;
     dg.Xuat();
 
-    cout << "Nhap goc de quay da giac: ";
+    cout << "Nhap goc de quay da giac (radian): ";
     cin >> x;
     dg.Quay(x);
     cout << "\nDa giac sau khi quay: " << endl;
