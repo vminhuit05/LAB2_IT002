3640690a91c15641e9f7572438f9821a71cf198d
Nguyễn Văn Minh <161409681+vminhuit05@users.noreply.github.com>
2024-10-14 23:32:27 +0700

    Update main.cpp

--- BAI1/main.cpp
+++ BAI1/main.cpp
@@ -3,24 +3,24 @@
 using namespace std;
 
 int main() {
-    Diem d1;  // Default constructor
+    Diem d1;
 
     double hoanh, tung;
-    cout << "Enter the coordinates for the point (x y): ";
+    cout << "Nhap toa do cho diem (x y): ";
     cin >> hoanh >> tung;
 
     d1.SetHoanhDo(hoanh);
     d1.SetTungDo(tung);
 
-    cout << "Point d1: ";
+    cout << "Diem d1: ";
     d1.Xuat();
 
     double dx, dy;
-    cout << "Enter the translation values (dx dy): ";
+    cout << "Nhap gia tri tinh tien (dx dy): ";
     cin >> dx >> dy;
     d1.TinhTien(dx, dy);
 
-    cout << "Point d1 after translation: ";
+    cout << "Diem d1 sau khi tinh tien: ";
     d1.Xuat();
 
     return 0;
